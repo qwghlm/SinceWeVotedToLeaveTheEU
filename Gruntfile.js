31b6861d6d3dccadb08396c2b123a795e8c8f38f
Chris Applegate <github@qwghlm.co.uk>
2016-06-28 20:29:10 +0100

    Add livereload & watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,12 +18,24 @@ module.exports = function(grunt) {
                     './css/main.css': './sass/main.scss'
                 }
             }
-        }
+        },
+
+        watch: {
+            scripts: {
+                files: ['sass/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                    livereload: true,
+                },
+            },
+        },
 
     });
 
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['sass']);
     grunt.registerTask('serve', ['shell:serve']);
