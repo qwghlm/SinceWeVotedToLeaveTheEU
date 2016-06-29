c365236e9c852065cc3bfa883755731f7f52569a
Chris Applegate <github@qwghlm.co.uk>
2016-06-29 22:08:01 +0100

    CSS animations, tweaks for mobile...

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    $('main').addClass('loaded');
+
     var referendumDay = new Date(2016, 5, 23);
     var days = Math.floor((new Date().getTime() - referendumDay)/(24*3600*1000));
     $('#days-since-referendum').html(days);
@@ -9,10 +11,10 @@ $(function() {
         scrollingSpeed : 600,
         onLeave : function(index, nextIndex, direction) {
             if (index === $('section').length - 1) {
-                $('footer').delay(600).animate({ height : "4em" });
+                $('footer').addClass('active')
             }
             else {
-                $('footer').delay(600).animate({ height : 0 });
+                $('footer').removeClass('active')
             }
         },
     });
